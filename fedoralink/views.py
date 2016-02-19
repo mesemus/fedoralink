87af27c3126ac38b7d575d068b445e53d4ea3e68
kosto1992 <kostelu1@fit.cvut.cz>
2016-02-19 12:57:02 +0100

    Pridane generic class do views vo fedoralinku

--- fedoralink/views.py
+++ fedoralink/views.py
@@ -1,13 +1,16 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, CreateView, DetailView, UpdateView
+
+from fedoralink.models import FedoraObject
 from .utils import get_class
 
 
 class GenericIndexerView(View):
 
-    model_class   = 'fedoralink.models.FedoraObject'
+    model   = FedoraObject
     template_name = 'fedoralink/indexer_view.html'
     base_template = 'please_set_base_template_for_generic_indexer_view'
     list_item_template = 'please_set_base_template_for_generic_indexer_view'
@@ -16,8 +19,8 @@ class GenericIndexerView(View):
     facets = None
 
     def get(self, request, parametry):
-        if isinstance(self.model_class, str):
-            self.model_class = get_class(self.model_class)
+        if isinstance(self.model, str):
+            self.model = get_class(self.model)
 
         if self.facets and callable(self.facets):
             requested_facets = self.facets(request, parametry)
@@ -27,7 +30,7 @@ class GenericIndexerView(View):
         requested_facet_ids   = [x[0] for x in requested_facets]
         requested_facet_names = [x[1] for x in requested_facets]
 
-        data = self.model_class.objects.all()
+        data = self.model.objects.all()
 
         if requested_facets:
             data = data.request_facets(*requested_facet_ids)
@@ -75,3 +78,57 @@ class GenericIndexerView(View):
             'ordering'      : sort
         })
 
+class GenericDocumentCreate(CreateView):
+    model = None
+    fields = '__all__'
+    template_name = None
+    parent_collection = None
+
+    def form_valid(self, form):
+        inst = form.save(commit=False)
+        inst.save()
+        self.object = inst
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_form_kwargs(self):
+        ret = super().get_form_kwargs()
+
+        if callable(self.parent_collection):
+            parent = self.parent_collection(self)
+        else:
+            parent = self.parent_collection
+
+        self.object = ret['instance'] = parent.create_child('', flavour=self.model)
+
+        return ret
+
+class GenericDetailView(DetailView):
+    model = None
+    prefix = None
+    template_name = None
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def get_object(self, queryset=None):
+        pk = self.prefix + self.kwargs.get(self.pk_url_kwarg, None).replace("_","/")
+        self.kwargs[self.pk_url_kwarg]=pk
+        print(self.kwargs)
+        return super().get_object(queryset)
+
+class GenericEditView(UpdateView):
+    model = None
+    fields = '__all__'
+    template_name = None
+
+    prefix = None
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def get_object(self, queryset=None):
+        pk = self.prefix + self.kwargs.get(self.pk_url_kwarg, None).replace("_","/")
+        self.kwargs[self.pk_url_kwarg]=pk
+        print(self.kwargs)
+        return super().get_object(queryset)
