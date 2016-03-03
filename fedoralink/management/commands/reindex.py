d52a4a26d9c4cb4ce474f9d69ee3da96b0871fa7
Miroslav Simek <miroslav.simek@gmail.com>
2016-03-03 09:36:43 +0100

    reindexing

---
+++ fedoralink/management/commands/reindex.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+from django.core.management import call_command
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+
+from fedoralink.models import FedoraObject
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Reindexuje cely obsah repozitare'
+
+    def handle(self, *args, **options):
+        obj = FedoraObject.objects.get(pk='')
+        indexer = connections['repository'].indexer
+        self.reindex(indexer, obj)
+
+    def reindex(self, indexer, obj, level=0):
+
+        if 'fedora:' in obj.id:
+            return
+
+        obj.update()
+
+        indexer.reindex(obj)
+
+        print("   " * level, obj.id, type(obj))
+
+        for c in obj.children:
+            self.reindex(indexer, c, level+1)
