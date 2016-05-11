794c06e73984044cb0cc184fd3e7d8625cdb065d
Mirek Simek <miroslav.simek@vscht.cz>
2016-05-11 13:51:45 +0200

    pycharm happy

--- fedoralink_ui/models.py
+++ fedoralink_ui/models.py
@@ -8,9 +8,8 @@ from fedoralink.indexer.models import IndexableFedoraObject
 
 
 class ResourceTypeCollection(IndexableFedoraObject):
-
     class Meta:
-        rdf_types = (CESNET_TYPE.ResourceTypeCollection, )
+        rdf_types = (CESNET_TYPE.ResourceTypeCollection,)
 
 
 class Template(IndexableFedoraObject):
@@ -19,48 +18,53 @@ class Template(IndexableFedoraObject):
     tags = IndexedTextField(CESNET_TYPE.tag, verbose_name=_('Tags'), multi_valued=True)
 
     class Meta:
-        rdf_types        = (CESNET_TYPE.Template, )
+        rdf_types = (CESNET_TYPE.Template,)
 
 
 class ResourceType(IndexableFedoraObject):
-
     label = IndexedTextField(CESNET_TYPE.label, verbose_name=_('Label'), level=IndexedField.MANDATORY)
 
     template_view = IndexedLinkedField(CESNET_TYPE.template_view, Template, verbose_name=_('Template for view'))
 
     template_edit = IndexedLinkedField(CESNET_TYPE.template_edit, Template, verbose_name=_('Template for edit'))
 
-    template_list_item = IndexedLinkedField(CESNET_TYPE.template_list_item, Template, verbose_name=_('Template for item list view'))
+    template_list_item = IndexedLinkedField(CESNET_TYPE.template_list_item, Template,
+                                            verbose_name=_('Template for item list view'))
 
-    controller = IndexedTextField(CESNET_TYPE.controller, verbose_name=_('Controller class'), level=IndexedField.MANDATORY)
+    controller = IndexedTextField(CESNET_TYPE.controller, verbose_name=_('Controller class'),
+                                  level=IndexedField.MANDATORY)
 
-    rdf_types = IndexedTextField(CESNET_TYPE.rdf_types, verbose_name=_('RDF types'), level=IndexedField.MANDATORY, multi_valued=True)
+    rdf_types = IndexedTextField(CESNET_TYPE.rdf_types, verbose_name=_('RDF types'), level=IndexedField.MANDATORY,
+                                 multi_valued=True)
 
     class Meta:
-        rdf_types = (CESNET_TYPE.ResourceType, )
+        rdf_types = (CESNET_TYPE.ResourceType,)
+
 
 class ResourceFieldType(IndexableFedoraObject):
-
     label = IndexedTextField(CESNET_TYPE.label, verbose_name=_('Label'), level=IndexedField.MANDATORY)
 
     containing_models = IndexedTextField(CESNET_TYPE.containing_models, verbose_name=_('Containing models'),
-                                 multi_valued=True)
+                                         multi_valued=True)
 
     field_name = IndexedTextField(CESNET_TYPE.field_name, verbose_name=_('Field name'))
 
-    field_fedoralink_type = IndexedTextField(CESNET_TYPE.field_fedoralink_type, verbose_name=_('Field fedoralink type, for example IndexedTextField'))
+    field_fedoralink_type = IndexedTextField(CESNET_TYPE.field_fedoralink_type,
+                                             verbose_name=_('Field fedoralink type, for example IndexedTextField'))
 
-    template_field_detail_view = IndexedLinkedField(CESNET_TYPE.template_field_detail_view, Template, verbose_name=_('Template for field inside detail view'))
+    template_field_detail_view = IndexedLinkedField(CESNET_TYPE.template_field_detail_view, Template,
+                                                    verbose_name=_('Template for field inside detail view'))
 
     class Meta:
         rdf_types = (CESNET_TYPE.ResourceFieldType,)
 
+
 class ResourceCollectionType(ResourceType):
-    templates_list= IndexedLinkedField(CESNET_TYPE.templates_list, Template,
-                                             verbose_name=_('Templates for list view'), multi_valued=True)
+    templates_list = IndexedLinkedField(CESNET_TYPE.templates_list, Template,
+                                        verbose_name=_('Templates for list view'), multi_valued=True)
 
     class Meta:
-        rdf_types = (CESNET_TYPE.ResourceCollectionType, )
+        rdf_types = (CESNET_TYPE.ResourceCollectionType,)
 
 
 class Controller():
