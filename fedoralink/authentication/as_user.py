d581b69c1816c7588d310472920673731355aa6d
Mirek Simek <miroslav.simek@vscht.cz>
2017-02-28 14:52:31 +0100

    as_admin, as_delegated_user context managers

--- fedoralink/authentication/as_user.py
+++ fedoralink/authentication/as_user.py
@@ -1,6 +1,7 @@
 import threading
 
 from fedoralink.authentication.Credentials import Credentials
+from fedoralink.middleware import FedoraUserDelegationMiddleware
 
 fedora_auth_local = threading.local()
 
@@ -17,3 +18,46 @@ class as_user:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         setattr(fedora_auth_local, 'Credentials', self.original_credentials)
+
+
+class as_delegated_user:
+    def __init__(self, username, groups):
+        self.username = username
+        self.groups   = groups
+
+    def __enter__(self):
+        if hasattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of'):
+            self.original_username = FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of
+            self.original_groups   = FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of_groups
+        else:
+            self.original_groups = self.original_username = None
+
+        FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of = self.username
+        FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of_groups = self.groups
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.original_username:
+            FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of = self.original_username
+            FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of_groups = self.original_groups
+        else:
+            delattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of')
+            delattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of_groups')
+
+
+class as_admin:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        if hasattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of'):
+            self.original_username = FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of
+            self.original_groups   = FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of_groups
+            delattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of')
+            delattr(FedoraUserDelegationMiddleware.thread_local_storage, 'fedora_on_behalf_of_groups')
+        else:
+            self.original_groups = self.original_username = None
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.original_username:
+            FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of = self.original_username
+            FedoraUserDelegationMiddleware.thread_local_storage.fedora_on_behalf_of_groups = self.original_groups
